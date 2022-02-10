19cb070f166ca3fbd7096472e551214bb250d2cc
David Lyons <davidbrucelyons@gmail.com>
2022-02-10 16:58:36 +0000

    Create bcl2fastq.sh

---
+++ bcl2fastq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -N 1 # number of nodes
+#SBATCH -n 20 # number of cores
+#SBATCH --mail-type=END,FAIL # notifications for job done & fail
+#SBATCH --mail-user=david.lyons@jic.ac.uk # send-to address
+#SBATCH --mem 40g # memory pool for all cores
+#SBATCH -p nbi-medium
+
+CONTAINER=/jic/software/production/bcl2fastq2/2.17.1.14/bcl2fastq2.img
+OUTPUT=/jic/scratch/groups/Daniel-Zilberman/Projects/dave/0-bcl_and_fq_jic/191212_demux
+
+##$CONTAINER --runfolder-dir /jic/scratch/groups/Daniel-Zilberman/Projects/dave/0-bcl_and_fq_jic/191212_NB501793_0129_AHJYLGBGXC --output-dir $OUTPUT --no-lane-splitting --use-bases-mask Y37,I6N,N7,Y37
+$CONTAINER --runfolder-dir /jic/scratch/groups/Daniel-Zilberman/Projects/dave/0-bcl_and_fq_jic/191212_NB501793_0129_AHJYLGBGXC  --output-dir $OUTPUT --no-lane-splitting
+
+#--use-bases-mask Y37,I6N,N7,Y37; this will 37bases, read 6 positions index while masking the 7th,
+#than completely skip the 2nd barcode (was acceidentally sequences), and then read the 2nd read.
